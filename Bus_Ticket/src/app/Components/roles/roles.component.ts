c463889935ab3247705bf5ae14934bca49c23098
Arguts Pužuls <aarguts12@gmail.com>
2025-02-03 22:33:36 +0200

    changes

--- Bus_Ticket/src/app/Components/roles/roles.component.ts
+++ Bus_Ticket/src/app/Components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/Interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -7,23 +9,66 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+
+  roleList: IRole [] = []
+  http = inject(HttpClient);
+
+// constructor(private http: HttpClient){
+
+// }
+
+ngOnInit(): void {
+  this.getAllRoles()
+}
+
+
+getAllRoles(){
+  this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+    this.roleList = res.data;
+
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   //string. number, boolean, date, object. array, null ,undefined
-firstName: string = 'Angular Tutorial';
-angularVersion = 'Version 19';
+// firstName: string = 'Angular Tutorial';
+// angularVersion = 'Version 19';
 
-version: number = 18;
-isActive : boolean = false;
-currentDate : Date = new Date();
-inputType: string = "button";
-selectedState: string = '';
+// version: number = 18;
+// isActive : boolean = false;
+// currentDate : Date = new Date();
+// inputType: string = "button";
+// selectedState: string = '';
 
-showWelcomeAlert () {
-  alert("Welcome to Angular 18 Tutorial")
-}
+// showWelcomeAlert () {
+//   alert("Welcome to Angular 18 Tutorial")
+// }
 
-showMessage(massage:string) {
-  alert(massage)
-}
+// showMessage(massage:string) {
+//   alert(massage)
+// }
 
 }
